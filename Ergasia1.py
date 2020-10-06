7aa7d77bf066126b4d2147d1776806bb8d7fb40a
stbakeas <72074556+stbakeas@users.noreply.github.com>
2020-10-06 13:15:27 +0300

    Update Ergasia1.py

--- Ergasia1.py
+++ Ergasia1.py
@@ -1,18 +1,22 @@
 import random
-matrix_dimension=3
+matrix_dimension=10
 
-#Matrix Creation
+#Δημιουργία του πίνακα
 basic_matrix=[[random.choice(("S","O")) for x in range(matrix_dimension)] for i in range(matrix_dimension)] 
 
-#Visual Representation of Matrix
+#Καλύτερη οπτική αναπαράσταση του πίνακα
 for i in basic_matrix: 
     print(i)
 
-new_list_horizontal=[]
+#Οι λίστες αυτές μας βοηθούν να εντοπίσουμε εάν υπάρχει κρυμμένο ένα SOS μέσα σε ένα άλλο.
+#π.χ. [S,O,S,O,S]. Εδώ υπάρχουν 2 SOS. Η new_list της προηγούμενης λίστας θα είναι:[SOS,OSO,SOS]
+new_list_horizontal=[] 
 new_list_vertical=[]
 new_list_diagonal=[]
 new_list_reverse_diagonal=[]
-horizontal_counter,vertical_counter,diagonal_counter,reverse_diagonal_counter=0,0,0,0
+
+#Μέτρηση SOS οριζόντια και κάθετα
+horizontal_counter,vertical_counter,diagonal_counter,reverse_diagonal_counter=0,0,0,0 
 for i in range(matrix_dimension):
   for j in range(matrix_dimension-2):
     new_list_horizontal.append(basic_matrix[i][j]+basic_matrix[i][j+1]+basic_matrix[i][j+2])
@@ -23,12 +27,14 @@ for i in range(matrix_dimension):
   new_list_horizontal.clear()
   new_list_vertical.clear()
 
+#Μέτρηση SOS κύριας διαγωνίου
 for i in range(matrix_dimension-2):
     for j in range(matrix_dimension-2):
      new_list_diagonal.append(basic_matrix[i][j]+basic_matrix[i+1][j+1]+basic_matrix[i+2][j+2])
     diagonal_counter+=new_list_diagonal.count("SOS")
     new_list_diagonal.clear()
 
+#Μέτρηση SOS δευτερεύουσας διαγωνίου
 for i in range(matrix_dimension-2):
   for j in range(matrix_dimension-1,1,-1):
     new_list_reverse_diagonal.append(basic_matrix[i][j]+basic_matrix[i+1][j-1]+basic_matrix[i+2][j-2])
