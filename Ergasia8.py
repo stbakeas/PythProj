14471d1ba6040a816819120a29f9ff9af925ac28
stbakeas <72074556+stbakeas@users.noreply.github.com>
2020-10-06 13:20:06 +0300

    Update Ergasia8.py

--- Ergasia8.py
+++ Ergasia8.py
@@ -5,7 +5,7 @@ total_number=0 #Συνόλο αριθμών που δημιουργήθηκαν 
 for i in range(0,100):
  amount_of_numbers=0 #Σύνολο αριθμών που δημιουργήθηκαν για έναν γύρο
  pawns_x_y=[0,0] #Αρχικοποίηση συντεταγμένων του πιονιού 
- direction=random.randint(1,4)
+ direction=random.randint(1,4) #Δημιουργία τυχαίων αριθμών
  
 
  if (direction==1):
@@ -43,7 +43,7 @@ for i in range(0,100):
      pawns_x_y[1]-=1
      amount_of_numbers+=1
 
- total_number+=amount_of_numbers
+ total_number+=amount_of_numbers 
  print(i+1,"Round Completed with:",amount_of_numbers,"numbers created")
   
 
